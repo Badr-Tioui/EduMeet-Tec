ea4eb79a9de99747220a39577fa994266744b200
unknown <badrtiwi493@gmail.com>
2026-01-08 08:30:32 +0100

    Final Proj

--- serverea/verify_dashboard.js
+++ serverea/verify_dashboard.js
@@ -5,7 +5,7 @@ require("dotenv").config({ path: "./.env" });
 
 const run = async () => {
     try {
-        const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/edumeet";
+        const mongoURI = process.env.MONGO_URIE || "mongodb://127.0.0.1:27017/edumeet";
         await mongoose.connect(mongoURI);
         console.log("Connected to DB");
 
